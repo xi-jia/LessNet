68e9d680c4d0de053410fe8071f75e23bd13a38e
Xi <57257260+xi-jia@users.noreply.github.com>
2025-04-15 13:58:16 +0100

    Add files via upload
    LessNet_C

--- 3D_LessNet/Models.py
+++ 3D_LessNet/Models.py
@@ -14,36 +14,25 @@ class UNet(nn.Module):
         bias_opt = True
 
         super(UNet, self).__init__()
-        self.eninput = self.encoder(self.in_channel, self.start_channel * 8, bias=bias_opt)
-        # self.ec1 = self.encoder(self.start_channel, self.start_channel, bias=bias_opt)
-        # self.ec2 = self.encoder(self.start_channel, self.start_channel * 2, stride=2, bias=bias_opt)
-        # self.ec3 = self.encoder(self.start_channel * 2, self.start_channel * 2, bias=bias_opt)
-        # self.ec4 = self.encoder(self.start_channel * 2, self.start_channel * 4, stride=2, bias=bias_opt)
-        # self.ec5 = self.encoder(self.start_channel * 4, self.start_channel * 4, bias=bias_opt)
-        # self.ec6 = self.encoder(self.start_channel * 4, self.start_channel * 8, stride=2, bias=bias_opt)
-        # self.ec7 = self.encoder(self.start_channel * 8, self.start_channel * 8, bias=bias_opt)
-        # self.ec8 = self.encoder(self.start_channel * 8, self.start_channel * 16, stride=2, bias=bias_opt)
-        # self.ec9 = self.encoder(self.start_channel * 16, self.start_channel * 8, bias=bias_opt)
+        self.eninput = self.encoder(self.in_channel, self.start_channel * 4, bias=bias_opt)
 
-        self.dc1 = self.encoder(self.start_channel * 8, self.start_channel * 8, kernel_size=3,
+        self.dc1 = self.encoder(self.start_channel * 4, self.start_channel * 4, kernel_size=3,
                                 stride=1, bias=bias_opt)
-        self.dc2 = self.encoder(self.start_channel * 8, self.start_channel * 8, kernel_size=3, stride=1, bias=bias_opt)
-        self.dc3 = self.encoder(self.start_channel * 6 + 6, self.start_channel * 6, kernel_size=3,
+        self.dc2 = self.encoder(self.start_channel * 4, self.start_channel * 4, kernel_size=3, stride=1, bias=bias_opt)
+        self.dc3 = self.encoder(self.start_channel * 3 + 6, self.start_channel * 3, kernel_size=3,
                                 stride=1, bias=bias_opt)
-        self.dc4 = self.encoder(self.start_channel * 6, self.start_channel * 6, kernel_size=3, stride=1, bias=bias_opt)
-        self.dc5 = self.encoder(self.start_channel * 4 + 6, self.start_channel * 4, kernel_size=3,
+        self.dc4 = self.encoder(self.start_channel * 3, self.start_channel * 3, kernel_size=3, stride=1, bias=bias_opt)
+        self.dc5 = self.encoder(self.start_channel * 2 + 6, self.start_channel * 2, kernel_size=3,
                                 stride=1, bias=bias_opt)
-        self.dc6 = self.encoder(self.start_channel * 4, self.start_channel * 4, kernel_size=3, stride=1, bias=bias_opt)
-        self.dc7 = self.encoder(self.start_channel * 2 + 2, self.start_channel * 2, kernel_size=3,
+        self.dc6 = self.encoder(self.start_channel * 2, self.start_channel * 2, kernel_size=3, stride=1, bias=bias_opt)
+        self.dc7 = self.encoder(self.start_channel * 1 + 2, self.start_channel * 1, kernel_size=3,
                                 stride=1, bias=bias_opt)
-        self.dc8 = self.encoder(self.start_channel * 2, self.start_channel * 2, kernel_size=3, stride=1, bias=bias_opt)
-        self.dc9 = self.outputs(self.start_channel * 2, self.n_classes, kernel_size=3, stride=1, padding=1, bias=False)
-        # self.dc10 = self.outputs(self.start_channel * 2, self.n_classes, kernel_size=5, stride=1, padding=2, bias=False)
-
-        # self.up1 = self.decoder(self.start_channel * 8, self.start_channel * 8)
-        self.up2 = self.decoder(self.start_channel * 8, self.start_channel * 6)
-        self.up3 = self.decoder(self.start_channel * 6, self.start_channel * 4)
-        self.up4 = self.decoder(self.start_channel * 4, self.start_channel * 2)
+        self.dc8 = self.encoder(self.start_channel * 1, self.start_channel * 1, kernel_size=3, stride=1, bias=bias_opt)
+        self.dc9 = self.outputs(self.start_channel * 1, self.n_classes, kernel_size=3, stride=1, padding=1, bias=False)
+        
+        self.up2 = self.decoder(self.start_channel * 4, self.start_channel * 3)
+        self.up3 = self.decoder(self.start_channel * 3, self.start_channel * 2)
+        self.up4 = self.decoder(self.start_channel * 2, self.start_channel * 1)
         
         self.maxp_1_2 = nn.MaxPool3d(2, stride=2)
         self.maxp_1_4 = nn.MaxPool3d(4, stride=4)
@@ -164,124 +153,6 @@ class UNet(nn.Module):
         
         
         return f_xy
-
-
-# class UNet(nn.Module):
-#     def __init__(self, in_channel, n_classes, start_channel):
-#         self.in_channel = in_channel
-#         self.n_classes = n_classes
-#         self.start_channel = start_channel
-
-#         bias_opt = True
-
-#         super(UNet, self).__init__()
-#         self.eninput = self.encoder(self.in_channel, self.start_channel, bias=bias_opt)
-#         self.ec1 = self.encoder(self.start_channel, self.start_channel, bias=bias_opt)
-#         self.ec2 = self.encoder(self.start_channel, self.start_channel * 2, stride=2, bias=bias_opt)
-#         self.ec3 = self.encoder(self.start_channel * 2, self.start_channel * 2, bias=bias_opt)
-#         self.ec4 = self.encoder(self.start_channel * 2, self.start_channel * 4, stride=2, bias=bias_opt)
-#         self.ec5 = self.encoder(self.start_channel * 4, self.start_channel * 4, bias=bias_opt)
-#         self.ec6 = self.encoder(self.start_channel * 4, self.start_channel * 8, stride=2, bias=bias_opt)
-#         self.ec7 = self.encoder(self.start_channel * 8, self.start_channel * 8, bias=bias_opt)
-#         self.ec8 = self.encoder(self.start_channel * 8, self.start_channel * 16, stride=2, bias=bias_opt)
-#         self.ec9 = self.encoder(self.start_channel * 16, self.start_channel * 8, bias=bias_opt)
-
-#         self.dc1 = self.encoder(self.start_channel * 8 + self.start_channel * 8, self.start_channel * 8, kernel_size=3,
-#                                   stride=1, bias=bias_opt)
-#         self.dc2 = self.encoder(self.start_channel * 8, self.start_channel * 4, kernel_size=3, stride=1, bias=bias_opt)
-#         self.dc3 = self.encoder(self.start_channel * 4 + self.start_channel * 4, self.start_channel * 4, kernel_size=3,
-#                                   stride=1, bias=bias_opt)
-#         self.dc4 = self.encoder(self.start_channel * 4, self.start_channel * 2, kernel_size=3, stride=1, bias=bias_opt)
-#         self.dc5 = self.encoder(self.start_channel * 2 + self.start_channel * 2, self.start_channel * 4, kernel_size=3,
-#                                   stride=1, bias=bias_opt)
-#         self.dc6 = self.encoder(self.start_channel * 4, self.start_channel * 2, kernel_size=3, stride=1, bias=bias_opt)
-#         self.dc7 = self.encoder(self.start_channel * 2 + self.start_channel * 1, self.start_channel * 2, kernel_size=3,
-#                                   stride=1, bias=bias_opt)
-#         self.dc8 = self.encoder(self.start_channel * 2, self.start_channel * 2, kernel_size=3, stride=1, bias=bias_opt)
-#         self.dc9 = self.outputs(self.start_channel * 2, self.n_classes, kernel_size=3, stride=1, padding=1, bias=False)
-#         self.dc10 = self.outputs(self.start_channel * 2, self.n_classes, kernel_size=3, stride=1, padding=1, bias=False)
-             
-#         self.up1 = self.decoder(self.start_channel * 8, self.start_channel * 8)
-#         self.up2 = self.decoder(self.start_channel * 4, self.start_channel * 4)
-#         self.up3 = self.decoder(self.start_channel * 2, self.start_channel * 2)
-#         self.up4 = self.decoder(self.start_channel * 2, self.start_channel * 2)
-
-#     def encoder(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1,
-#                 bias=False, batchnorm=False):
-#         if batchnorm:
-#             layer = nn.Sequential(
-#                 # nn.Dropout(0.1),
-#                 nn.Conv3d(in_channels, out_channels, kernel_size, stride=stride, padding=padding, bias=bias),
-#                 nn.BatchNorm3d(out_channels),
-#                 nn.PReLU())
-#         else:
-#             layer = nn.Sequential(
-#                 # nn.Dropout(0.1),
-#                 nn.Conv3d(in_channels, out_channels, kernel_size, stride=stride, padding=padding, bias=bias),
-#                 nn.PReLU())
-#         return layer
-
-#     def decoder(self, in_channels, out_channels, kernel_size=2, stride=2, padding=0,
-#                 output_padding=0, bias=True):
-#         layer = nn.Sequential(
-#             # nn.Dropout(0.1),
-#             nn.ConvTranspose3d(in_channels, out_channels, kernel_size, stride=stride,
-#                                padding=padding, output_padding=output_padding, bias=bias),
-#             nn.PReLU())
-#         return layer
-
-#     def outputs(self, in_channels, out_channels, kernel_size=3, stride=1, padding=0,
-#                 bias=False, batchnorm=False):
-#         if batchnorm:
-#             layer = nn.Sequential(
-#                 nn.Conv3d(in_channels, out_channels, kernel_size, stride=stride, padding=padding, bias=bias),
-#                 nn.BatchNorm3d(out_channels),
-#                 nn.Tanh())
-#         else:
-#             layer = nn.Sequential(
-#                 nn.Conv3d(in_channels, out_channels, kernel_size, stride=stride, padding=padding, bias=bias),
-#                 nn.Softsign())
-#         return layer
-
-#     def forward(self, x, y):
-#         x_in = torch.cat((x, y), 1)
-#         e0 = self.eninput(x_in)
-#         e0 = self.ec1(e0)
-
-#         e1 = self.ec2(e0)
-#         e1 = self.ec3(e1)
-
-#         e2 = self.ec4(e1)
-#         e2 = self.ec5(e2)
-
-#         e3 = self.ec6(e2)
-#         e3 = self.ec7(e3)
-
-#         e4 = self.ec8(e3)
-#         e4 = self.ec9(e4)
-
-#         d0 = torch.cat((self.up1(e4), e3), 1)
-
-#         d0 = self.dc1(d0)
-#         d0 = self.dc2(d0)
-
-#         d1 = torch.cat((self.up2(d0), e2), 1)
-
-#         d1 = self.dc3(d1)
-#         d1 = self.dc4(d1)
-
-#         d2 = torch.cat((self.up3(d1), e1), 1)
-
-#         d2 = self.dc5(d2)
-#         d2 = self.dc6(d2)
-
-#         d3 = torch.cat((self.up4(d2), e0), 1)
-#         d3 = self.dc7(d3)
-#         d3 = self.dc8(d3)
-        
-#         f_r = self.dc9(d3)
-        
-#         return f_r
 
 
 class SpatialTransform(nn.Module):
